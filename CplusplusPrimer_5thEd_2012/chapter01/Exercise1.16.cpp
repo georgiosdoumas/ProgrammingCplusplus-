586a167c03d607eb6c5b71755b0b206e9c4e90ab
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2016-05-20 20:19:12 +0200

    Create Exercise1.16.cpp

---
+++ CplusplusPrimer_5thEd_2012/chapter01/Exercise1.16.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+    int sum = 0;
+    std::cout << "Enter a series of integers seperated by space or [enter],when done type Ctrl+d to end input: " ;
+    //An example of for loop that has "initial-statelent",has "condition" , but no "expression" 
+    //Even the initial statement could be absent if I had declared int value along with sum 
+    //for( ;std::cin>>value; )
+    for (int value=0;std::cin >> value;)
+        sum += value; 
+    std::cout << "Sum of given numbers is: " << sum << std::endl;
+    return 0;
+}
