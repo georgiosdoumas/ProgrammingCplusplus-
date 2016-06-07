a0275deb456d425cf2f1f1eb41eaeeaa2f6d5cf3
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2016-06-07 21:12:26 +0200

    Create 3.6geometry.h

---
+++ NavigatingCppAndObjectOrientedDesign/chap03/3.6geometry.h
@@ -0,0 +1,9 @@
+#ifndef GEOMETRYH
+#define GEOMETRYH
+// 3.6geometry.h - geometry definitions
+namespace Geometry 					// namespace definition
+{
+	 struct Point {	double x, y;  };
+	 double slope(Point, Point);       //function declaration , for definition see file 3.6geometry.cpp
+}
+#endif
