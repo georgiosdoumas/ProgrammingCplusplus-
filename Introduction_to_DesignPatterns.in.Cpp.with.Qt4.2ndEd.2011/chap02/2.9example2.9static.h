1393f60f51017cb5cab0b0cda2d1edcbf7ad12e2
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2016-08-08 23:46:18 +0200

    Create 2.9example2.9static.h

---
+++ Introduction_to_DesignPatterns.in.Cpp.with.Qt4.2ndEd.2011/chap02/2.9example2.9static.h
@@ -0,0 +1,11 @@
+class Thing 
+{
+public:
+    Thing(int a, int b);
+    ~Thing();
+    void display() const ;
+    static void showCount();
+private:
+    int m_First, m_Second;
+    static int s_Count;     //count existing Things
+};
