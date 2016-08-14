a19d524261ab3f8d47ce1516b5546ea1fbe8a1e0
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2016-08-14 17:40:24 +0200

    Create exercise06.cpp

---
+++ Thinking_in_Cpp_vol1/chap02/exercise06.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main() 
+{
+  vector<string> vlines;
+  ifstream fin("text06.txt");
+  string line,line_whole_text;
+  while(getline(fin, line))
+    vlines.push_back(line); // Each line is another i-th element of the vector vlines[i]
+  for(int i = 0; i < vlines.size(); i++) 
+  { 
+     line_whole_text+=vlines[i]; 
+     line_whole_text+=" ";   // or : line_whole_text+="\n";  //if we want to output new text lines normally
+  }
+  cout << line_whole_text << endl;
+}
+
+/* *******
+line one
+second line here.
+3rd line is this.
+line number four, final line.
+****** */
