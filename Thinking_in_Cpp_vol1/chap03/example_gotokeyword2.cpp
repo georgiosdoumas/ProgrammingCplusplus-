303dcb3098dd33e6fbd34f317fee874ae3620cb2
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2016-08-14 15:22:26 +0200

    Create example_gotokeyword2.cpp

---
+++ Thinking_in_Cpp_vol1/chap03/example_gotokeyword2.cpp
@@ -0,0 +1,29 @@
+//: C03:gotoKeyword.cpp
+// The infamous goto is supported in C++
+//but here is another alternative solution.
+//I used the advice mentioned in the book/web-page , but it is not well expressed.
+//It says in the text 
+//"The alternative would be to set a Boolean that is tested in the outer for loop, and then do a break from the inner for loop."
+//but in reality it should say :
+//The alternative would be to set a Boolean that is tested in the inner for loop, and then do a break from the outer for loop.
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+  long val = 0;
+  for(int i = 1; i < 1000; i++) 
+  {
+    bool is47000 = false;
+    cout << "i="<<i<<endl;
+    for(int j = 1; j < 100; j += 10) 
+    {
+      val = i * j;
+      cout<<"  j="<<j;
+      if(val > 47000)  is47000 = true; //we reached the value we want
+    }
+    cout<<endl;
+    if(is47000==true) break; //exit loop and print val
+  }
+  cout << "\nval=" << val << endl;
+} 
