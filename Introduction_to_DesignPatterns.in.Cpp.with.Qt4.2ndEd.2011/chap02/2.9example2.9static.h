e2cfa81ff8640bfdc729829749de19bf2986e151
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2016-08-14 18:00:04 +0200

    Update 2.9example2.9static.h

--- Introduction_to_DesignPatterns.in.Cpp.with.Qt4.2ndEd.2011/chap02/2.9example2.9static.h
+++ Introduction_to_DesignPatterns.in.Cpp.with.Qt4.2ndEd.2011/chap02/2.9example2.9static.h
@@ -3,9 +3,9 @@ class Thing
 public:
     Thing(int a, int b);
     ~Thing();
-    void display() const ;
+    void display() const ;  //for implementations-definitions, look file 2.9example2.10static.cpp
     static void showCount();
 private:
     int m_First, m_Second;
-    static int s_Count;     //count existing Things
+    static int s_Count;     //counts existing Things
 };
