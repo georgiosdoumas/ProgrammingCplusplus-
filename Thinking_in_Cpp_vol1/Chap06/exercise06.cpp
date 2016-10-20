1ddb14cca6f693f4965cc829d51df868a16ed595
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2016-10-20 23:43:41 +0200

    Create exercise06.cpp

---
+++ Thinking_in_Cpp_vol1/Chap06/exercise06.cpp
@@ -0,0 +1,27 @@
+//: C06:Handle.cpp {O}
+// Handle implementation for exercise 6
+#include "Handle.h"
+#include "../require.h"
+#include <iostream>
+// Define Handle's implementation:
+struct Handle::Cheshire 
+{
+  int i;
+  std::string word;
+};
+
+Handle::Handle()
+{
+  smile = new Cheshire;
+  smile->i = 0;
+  smile->word = "";
+}
+
+Handle::~Handle()
+{  std::cout << "Destructor will delete the member struct! Trust the destructor" << std:: endl; delete smile; }
+
+int Handle::read() 
+{  std::cout<< smile->word << std::endl; return smile->i; }
+
+void Handle::change(int x) 
+{  smile->i = x; smile->word = "changed"; } 
