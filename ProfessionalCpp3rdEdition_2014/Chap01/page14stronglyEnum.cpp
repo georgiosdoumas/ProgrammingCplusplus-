9d7fcec747675eda658eb9b27fc214263291af72
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-10-27 22:11:38 +0200

    Create page14stronglyEnum.cpp
    I wanted to be sure that enumerators is not something we can simply pass cout streams.
    So the way to print their values is at this point of the book still unknown. The reader has no clue.
    But after page 16 (where the switch is presented ) one possible way is apparent. See page16switchExample.cpp

---
+++ ProfessionalCpp3rdEdition_2014/Chap01/page14stronglyEnum.cpp
@@ -0,0 +1,15 @@
+// Professional C++ 3rd Ed , Chap 01 page 14 Strongly typed enumerators 
+#include <iostream>
+using namespace std;
+int main()
+{
+    enum class MyEnum { EnumValue1, EnumValue2 = 10, EnumValue3 };
+    enum class charEnum: char { enumchar_d = 'd' , enumchar_f = 'f' }; //I wanted to see if it also compiles, and it does!
+    MyEnum variable1 = MyEnum::EnumValue3;
+    //cout << variable1 << endl;   //compiler error
+    charEnum leter1 = charEnum::enumchar_f ;
+    //cout << leter1 << endl;      //compiler error
+    //So we do not know yet how to print the values of these enumerators
+    return 0;
+}
+// g++ -Wall -std=c++11 -o page14stronglyEnum  page14stronglyEnum.cpp
