86d7b0fbb212d78f42abc35e9faa2e6b8c99a5b0
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-10-27 22:03:16 +0200

    Create page09hellovariables.cpp

---
+++ ProfessionalCpp3rdEdition_2014/Chap01/page09hellovariables.cpp
@@ -0,0 +1,14 @@
+// Professional C++ 3rd Ed , Chap 01 page 9 Variables
+#include <iostream>
+using namespace std;
+int main()
+{
+	int uninitializedInt;
+	int initializedInt = 7;
+	cout << uninitializedInt << " is a random value" << endl;	//it gives a 0 value. The compiler warned me but initiallized it to 0
+	cout << initializedInt << " was assigned an initial value" << endl;
+	return (0);
+}
+
+// g++ -Wall -std=c++11 -o page09hellovariables page09hellovariables.cpp
+// ./page09hellovariables
