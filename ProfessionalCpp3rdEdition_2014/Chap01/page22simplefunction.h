d3913bef79a010f35fc00d3e0b80299917fb57c2
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-10-27 23:29:06 +0200

    Create page22simplefunction.h

---
+++ ProfessionalCpp3rdEdition_2014/Chap01/page22simplefunction.h
@@ -0,0 +1,9 @@
+// Professional C++ 3rd Ed , Chap 01 page 22-23
+//header file for a simple fuction declaration
+//For implementation look at page22simplefunction.cpp 
+//the main() will be at page22main.cpp
+
+void charactermultiplier(int i, char c);  
+//instead of the myFunction that the book has, I devised something more interesting! 
+
+int addNumbers(int number1, int number2);
