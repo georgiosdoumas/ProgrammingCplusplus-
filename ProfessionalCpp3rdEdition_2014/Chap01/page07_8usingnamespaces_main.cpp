b636fdf29893cd4196a35ca784b9abf3f3314519
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-10-27 21:59:57 +0200

    Create page07_8usingnamespaces_main.cpp
    The main() program , and the commands to compile it in linux, using g++ , producing the executable page07_8usingnamespaces

---
+++ ProfessionalCpp3rdEdition_2014/Chap01/page07_8usingnamespaces_main.cpp
@@ -0,0 +1,10 @@
+// Professional C++ 3rd Ed , Chap 01 page 7-8
+#include "page07_8namespaces.h"
+using namespace mycode_ns;
+int main()
+{
+	foo(); // Implies mycode_ns::foo();
+	return 0;
+}
+//g++ -o page07_8usingnamespaces page07_8usingnamespaces_functions.cpp page07_8usingnamespaces_main.cpp
+//  ./page07_8usingnamespaces
