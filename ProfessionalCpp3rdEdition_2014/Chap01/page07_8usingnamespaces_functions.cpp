1b84dd1a915853c6e8dff6e3e13494c1505f7649
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-10-27 21:56:57 +0200

    Create page07_8usingnamespaces_functions.cpp
    The source file for the function implementation of this namespace

---
+++ ProfessionalCpp3rdEdition_2014/Chap01/page07_8usingnamespaces_functions.cpp
@@ -0,0 +1,11 @@
+// Professional C++ 3rd Ed , Chap 01 page 7-8
+// no need to include <iostream> since it is included through the header 
+#include "page07_8namespaces.h"  //function declaration of foo is on that header file. Implementation/definition is here
+using std::cout;
+namespace mycode_ns
+{
+	void foo() 
+	{
+		cout << __func__ << " called in the mycode_ns namespace" << std::endl; //the __func__ is presented at page 22-23
+	}
+}
