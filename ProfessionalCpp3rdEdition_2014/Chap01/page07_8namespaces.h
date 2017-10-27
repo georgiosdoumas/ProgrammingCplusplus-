cb751575cee9d6d2a4b238520400a0d7f602832d
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-10-27 21:55:26 +0200

    Create page07_8namespaces.h
    The header to be included in the other 2 source files : 
    page07_8namespaces_functions.cpp 
    page07_8usingnamespaces_main.cpp

---
+++ ProfessionalCpp3rdEdition_2014/Chap01/page07_8namespaces.h
@@ -0,0 +1,9 @@
+// Professional C++ 3rd Ed , Chap 01 page 7-8
+#ifndef Page7_8namespaces_h
+#define Page7_8namespaces_h
+#include <iostream> 
+namespace mycode_ns 
+{
+	void foo();		//function declaration. For implementetion look at source file page07_8namespaces_functions.cpp
+}
+#endif
