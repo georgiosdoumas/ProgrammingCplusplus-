651a058f449a6967b5faf1576c7eff7e09993195
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-10-27 22:38:25 +0200

    Create page15employeestruct.h

---
+++ ProfessionalCpp3rdEdition_2014/Chap01/page15employeestruct.h
@@ -0,0 +1,11 @@
+// Professional C++ 3rd Ed , Chap 01 page 15 header file with struct definition
+#ifndef EmployeeStruct_h
+#define EmployeeStruct_h
+struct Employee {
+    char firstInitial;
+    char lastInitial;
+    int employeeNumber;
+    float salary;
+};
+#endif
+// header file to be included in page15structEmployee.cpp
