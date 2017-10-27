9232c3c4703fa6421869078c22a399022e6fc725
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-10-27 22:30:28 +0200

    Update page14stronglyEnum.cpp

--- ProfessionalCpp3rdEdition_2014/Chap01/page14stronglyEnum.cpp
+++ ProfessionalCpp3rdEdition_2014/Chap01/page14stronglyEnum.cpp
@@ -9,7 +9,7 @@ int main()
     //cout << variable1 << endl;   //compiler error
     charEnum leter1 = charEnum::enumchar_f ;
     //cout << leter1 << endl;      //compiler error
-    //So we do not know yet how to print the values of these enumerators
+    //So we do not know yet how to print the values of these enumerators (but see page16switchExample.cpp )
     return 0;
 }
 // g++ -Wall -std=c++11 -o page14stronglyEnum  page14stronglyEnum.cpp
