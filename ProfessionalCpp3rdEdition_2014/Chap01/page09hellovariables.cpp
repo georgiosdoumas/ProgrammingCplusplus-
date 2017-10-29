4445544aef701a572f2705203aadf49e266b44d0
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-10-29 22:04:30 +0100

    Update page09hellovariables.cpp

--- ProfessionalCpp3rdEdition_2014/Chap01/page09hellovariables.cpp
+++ ProfessionalCpp3rdEdition_2014/Chap01/page09hellovariables.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 int main()
 {
-	int uninitializedInt;
-	int initializedInt = 7;
-	cout << uninitializedInt << " is a random value" << endl;	//it gives a 0 value. The compiler warned me but initiallized it to 0
-	cout << initializedInt << " was assigned an initial value" << endl;
-	return (0);
+    int uninitializedInt;
+    int initializedInt = 7;
+    cout << uninitializedInt << " is a random value" << endl;	//it gives a 0 value. Compiler warned but initiallized it to 0
+    cout << initializedInt << " was assigned an initial value" << endl;
+    return (0);
 }
 
 // g++ -Wall -std=c++11 -o page09hellovariables page09hellovariables.cpp
