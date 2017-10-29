73a76405ef03388b54534863b5decc7d41561cd5
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-10-29 22:03:25 +0100

    Update page07_8usingnamespaces_main.cpp

--- ProfessionalCpp3rdEdition_2014/Chap01/page07_8usingnamespaces_main.cpp
+++ ProfessionalCpp3rdEdition_2014/Chap01/page07_8usingnamespaces_main.cpp
@@ -1,4 +1,4 @@
-// Professional C++ 3rd Ed , Chap 01 page 7-8
+// Professional C++ 3rd Ed , Chap 01 page 7-8, a complete working example of namespaces
 #include "page07_8namespaces.h"
 using namespace mycode_ns;
 int main()
