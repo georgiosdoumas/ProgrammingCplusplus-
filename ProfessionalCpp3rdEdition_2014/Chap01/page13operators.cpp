981770b95676aafb659e673d4dbf8d14e146675e
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-10-29 22:06:35 +0100

    Update page13operators.cpp

--- ProfessionalCpp3rdEdition_2014/Chap01/page13operators.cpp
+++ ProfessionalCpp3rdEdition_2014/Chap01/page13operators.cpp
@@ -1,6 +1,6 @@
 // Professional C++ 3rd Ed , Chap 01 page 13
 #include <iostream>
-using std::cout; using std::cin; 
+using std::cout; using std::cin; using std::endl;
 int main()
 {
     int someInteger = 256;
@@ -11,13 +11,13 @@ int main()
     someInteger++;  //257
     someInteger *= 2;  //514
     someShort = static_cast<short>(someInteger); //
-    cout << "After casting the integer " << someInteger << " to a short, we do not lose anything, still have : " << someShort << std::endl;
+    cout <<"After casting the integer "<< someInteger <<" to a short, we do not loose anything, still have :"<< someShort << endl;
     someLong = someShort * 10000;
-    cout << "Now we will add 0.785 + " << someLong << std::endl;
+    cout << "Now we will add 0.785 + " << someLong << endl;
     someFloat = someLong + 0.785f;
-    cout << someFloat << " , so we lost the information of 0.785. Lets divide by 100000" << std::endl;
+    cout << someFloat << " , so we lost the information of 0.785. Lets divide by 100000" << endl;
     someDouble = static_cast<double>(someFloat) / 100000;
-    cout << someDouble << std::endl;
+    cout << someDouble << endl;
 }
 // g++ -Wall -std=c++11 -o page13operators page13operators.cpp
 // ./page13operators
