66a3110cfbcadf1b7bf62bfa7c9dfb4ddd436959
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-10-29 22:07:46 +0100

    Update page14a_typedef_enums.cpp

--- ProfessionalCpp3rdEdition_2014/Chap01/page14a_typedef_enums.cpp
+++ ProfessionalCpp3rdEdition_2014/Chap01/page14a_typedef_enums.cpp
@@ -1,12 +1,12 @@
-// Professional C++ 3rd Ed , Chap 01 page 14
+// Professional C++ 3rd Ed , Chap 01 page 14, trying out the enum 
 #include <iostream>
 int main()
 {
-	enum PieceType { PieceTypeKing, PieceTypeQueen, PieceTypeRook, PieceTypePawn }; //no need for: typedef enum PieceType , as in C
-	PieceType somePiece;
-	//somePiece = 0; 	//compiler error, so we have to use the next line
-	somePiece = PieceTypeKing; //only this is acceptable now!
-	PieceType another = PieceTypePawn; //it has value 3 
-	return 0;
+    enum PieceType { PieceTypeKing, PieceTypeQueen, PieceTypeRook, PieceTypePawn }; //no need for: typedef enum PieceType , as in C
+    PieceType somePiece;
+    //somePiece = 0; 	//compiler error, so we have to use the next line
+    somePiece = PieceTypeKing; //only this is acceptable now!
+    PieceType another = PieceTypePawn; //it has value 3 
+    return 0;
 }
 //g++ -o page14a_typedef_enums page14a_typedef_enums.cpp
