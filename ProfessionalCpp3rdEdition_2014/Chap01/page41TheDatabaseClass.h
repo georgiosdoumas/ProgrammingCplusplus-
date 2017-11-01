663018288a630ada75b26135605172a3f4839f07
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-11-01 01:06:02 +0100

    Create page41TheDatabaseClass.h

---
+++ ProfessionalCpp3rdEdition_2014/Chap01/page41TheDatabaseClass.h
@@ -0,0 +1,24 @@
+// Professional C++ 3rd Ed, Chap 01 page 41 header file for class Database
+// NOTE: where the book says mNextEmployeeNumber , I decided to say mNewHireEmployeeNumber
+#pragma once   // instead of using #ifndef and similar ...
+#include <iostream>
+#include <vector>
+#include "page38Employee.h"
+namespace Records 
+{
+  const int kFirstEmployeeNumber = 1000;  //each new hired employee  increases it. Fired ones will NOT decrease it!
+  class Database
+  {
+    public:
+      Database();
+      Employee& addEmployee(const std::string& firstName, const std::string& lastName);
+      Employee& getEmployee(int employeeNumber);
+      Employee& getEmployee(const std::string& firstName, const std::string& lastName);
+      void displayAll() const;
+      void displayCurrent() const;
+      void displayFormer() const;
+    private:
+      std::vector<Employee> mEmployees;
+      int mNewHireEmployeeNumber;   //tracks the number of last hired
+  };
+}
