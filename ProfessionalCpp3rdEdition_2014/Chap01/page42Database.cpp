5f7123ca517ba3595a792ac62eae99b8ae792380
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-11-02 01:02:53 +0100

    Update page42Database.cpp

--- ProfessionalCpp3rdEdition_2014/Chap01/page42Database.cpp
+++ ProfessionalCpp3rdEdition_2014/Chap01/page42Database.cpp
@@ -2,7 +2,7 @@
 //NOTE: again I follow my naming convention, where the book says theEmployee I say newEmployee
 #include "page41TheDatabaseClass.h"
 #include <iostream>
-#include <stdexcept>
+#include <stdexcept>  
 using namespace std;
 namespace Records 
 {
@@ -27,7 +27,8 @@ namespace Records
          { return employee; }
     }
     // No return has happened in the for-loop? No matching employee was found. Throw exception :
-    throw runtime_error("No employee found with ID: ");  //TO_DO 
+    throw runtime_error("No employee found with ID: "); 
+    cout << employeeNumber ;        //my addition , does not exist in the book
   }
   // My implementetion for the other method of getting 
   Employee& Database::getEmployee(const std::string& firstName, const std::string& lastName)
@@ -37,7 +38,8 @@ namespace Records
         if (employee.getFirstName() == firstName && employee.getLastName() == lastName)
          { return employee; }
     }  
-    throw runtime_error("No employee found with name: ");  //TO_DO 
+    throw runtime_error("No employee found with name: "); 
+    cout << firstName + " " + lastName ;  //my addition , does not exist in the book
   }
   
   void Database::displayAll() const
