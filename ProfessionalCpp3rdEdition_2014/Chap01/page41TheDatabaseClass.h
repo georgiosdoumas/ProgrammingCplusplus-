471ec3f7c38a62c8eb931bc9152b30643012b766
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-11-03 00:30:19 +0100

    Update page41TheDatabaseClass.h

--- ProfessionalCpp3rdEdition_2014/Chap01/page41TheDatabaseClass.h
+++ ProfessionalCpp3rdEdition_2014/Chap01/page41TheDatabaseClass.h
@@ -1,7 +1,7 @@
-// Professional C++ 3rd Ed, Chap 01 page 41 header file for class Database
+// Professional C++ 3rd Ed, Chap 01 page 41 header file for class Database. 
 // Functions will be defined on page42Database.cpp
 // NOTE: where the book says mNextEmployeeNumber , I decided to say mNewHireEmployeeNumber
-#pragma once   // instead of using #ifndef and similar ...
+#pragma once   // instead of using #ifndef etc to ensure single inclusion of header files
 #include <iostream>
 #include <vector>
 #include "page38Employee.h"
@@ -11,7 +11,7 @@ namespace Records
   class Database
   {
     public:
-      Database();
+      Database();   // here is the constructor, but where is the destructor ? I hope chapter 7 will discuss these things
       Employee& addEmployee(const std::string& firstName, const std::string& lastName);
       Employee& getEmployee(int employeeNumber);
       Employee& getEmployee(const std::string& firstName, const std::string& lastName);
