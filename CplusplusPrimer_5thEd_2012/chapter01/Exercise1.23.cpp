8b2f59a378161502f6fb53304ff38c37f4641d8b
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-11-04 00:18:37 +0100

    Update Exercise1.23.cpp

--- CplusplusPrimer_5thEd_2012/chapter01/Exercise1.23.cpp
+++ CplusplusPrimer_5thEd_2012/chapter01/Exercise1.23.cpp
@@ -1,3 +1,5 @@
+/* Exercise 1.23: Write a program that reads several transactions and counts how many
+transactions occur for each ISBN . */ 
 #include <iostream>
 #include "Sales_item.h"
 //Using the example program of 1.4.4 as a guide 
@@ -7,9 +9,9 @@ int main()
     if(std::cin >> current_item) //read first transaction 
     {
         int cnt=1;
-        while (std::cin >> running_item) //read next line,it may contain a book of similar ISBN ...
+        while (std::cin >> running_item) //read next line,it may contain a book of  ...
         {
-            if( running_item.isbn() == current_item.isbn() )   
+            if( running_item.isbn() == current_item.isbn() )   //... similar ISBN to the previous transaction...
                 ++cnt; 
             else                        //... or new ISBN 
             {    
@@ -17,7 +19,7 @@ int main()
                 current_item=running_item; 
                 cnt=1;
             }
-        }  //after reading last line , do not forget to output the final bunch (1 or more) of ISBN
+        }  //after reading last line , do not forget to output the final bunch (1 or more transactions) of ISBN
         std::cout << "And we have " << cnt << " transactions for " << current_item.isbn() << std::endl;
     }
     else std::cout << "No data to process" << std::endl;  // in case of an empty file
