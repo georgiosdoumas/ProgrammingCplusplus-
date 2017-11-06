1f864f318b87d85e091de3031dc91d9450e7050f
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2017-11-06 23:02:03 +0100

    Update page14a_typedef_enums.cpp

--- ProfessionalCpp3rdEdition_2014/Chap01/page14a_typedef_enums.cpp
+++ ProfessionalCpp3rdEdition_2014/Chap01/page14a_typedef_enums.cpp
@@ -1,12 +1,17 @@
-// Professional C++ 3rd Ed , Chap 01 page 14, trying out the enum 
+// Professional C++ 3rd Ed , Chap 01 page 14
 #include <iostream>
 int main()
 {
-    enum PieceType { PieceTypeKing, PieceTypeQueen, PieceTypeRook, PieceTypePawn }; //no need for: typedef enum PieceType, as in C
-    PieceType somePiece;
-    //somePiece = 0; 	//compiler error, so we have to use the next line
-    somePiece = PieceTypeKing; //only this is acceptable now!
-    PieceType another = PieceTypePawn; //it has value 3 
-    return 0;
+	enum PieceType {PieceTypeKing=1, PieceTypeQueen, PieceTypeRook=10, PieceTypePawn}; //no need for: typedef enum PieceType, as in C
+	PieceType somePiece;
+	//somePiece = 1; 	//compiler error, so we have to use the next line
+	somePiece = PieceTypeKing; //only this is acceptable now!
+	PieceType anotherPiece = PieceTypePawn; //it has value 11 , next value of PieceTypeRook 
+	std::cout << somePiece << std::endl;  //this also compiles! 
+	std::cout << PieceTypeQueen << std::endl;
+	//We can also use more strictly these casts, presented in chapter 10, but in other books like C++ Primer (or Beginning C++) they appear in early chapters
+	std::cout << static_cast<int> (somePiece) << " is the value of somePiece." << std::endl; 
+	std::cout << static_cast<int> (anotherPiece) << " is the value of anotherPiece." << std::endl;
+	return 0;
 }
-//g++ -o page14a_typedef_enums page14a_typedef_enums.cpp
+//g++ -Wall -std=c++11 -o page14a_typedef_enums page14a_typedef_enums.cpp
