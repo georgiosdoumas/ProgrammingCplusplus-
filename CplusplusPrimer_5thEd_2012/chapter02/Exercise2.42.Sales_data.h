df2ec925bd5d6d1f85300c934afcc81cb52e2173
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-01-28 11:38:13 +0100

    Create Exercise2.42.Sales_data.h

---
+++ CplusplusPrimer_5thEd_2012/chapter02/Exercise2.42.Sales_data.h
@@ -0,0 +1,14 @@
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+
+#include <string>
+struct Sales_data {
+    std::string bookISBN;
+    std::string bookTitle;
+    unsigned units_sold = 0;  
+    // unsigned units_sold; /* in case you want to compile with pre c++11 , do not initialize */
+    double revenue = 0.0;     
+    // double revenue ; /* in case you want to compile with pre c++11 , do not initialize */
+};
+
+#endif
