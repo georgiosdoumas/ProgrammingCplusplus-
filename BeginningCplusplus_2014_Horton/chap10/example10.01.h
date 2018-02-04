5df0e815f939019fc03150df030c69f2c761e8bd
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-02-04 11:32:31 +0100

    Create example10.01.h

---
+++ BeginningCplusplus_2014_Horton/chap10/example10.01.h
@@ -0,0 +1,11 @@
+// Ex10_01.h
+// Definitions for globals in namespace data
+#include <string>
+#ifndef EX10_01_H
+#define EX10_01_H
+namespace data
+{
+	extern const double pi {3.14159265};
+	extern const std::string days[] {"Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"};
+}
+#endif
