e0d4be8f163fdf662f7a8e5b35bf697971359049
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-02-04 15:37:01 +0100

    Create example10.02compare.h

---
+++ BeginningCplusplus_2014_Horton/chap10/headers/example10.02compare.h
@@ -0,0 +1,12 @@
+// For Ex10_02.cpp
+#include <vector>
+#ifndef COMPARE_H
+#define COMPARE_H
+ 
+namespace compare
+{
+	using std::vector;
+	double max(const vector<double>& data);
+	double min(const vector<double>& data);
+}
+#endif
