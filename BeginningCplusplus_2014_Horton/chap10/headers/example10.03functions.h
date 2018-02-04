25edf69de73fa245279060eb6ba4bc9e6c2b7d25
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-02-04 22:43:01 +0100

    Create example10.03functions.h

---
+++ BeginningCplusplus_2014_Horton/chap10/headers/example10.03functions.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+namespace fun
+{
+// Function prototypes inside the namespace
+	int sum(int, int);		// Sum arguments
+	int product(int, int);		// Product of arguments
+	int difference(int, int);		// Difference between arguments
+}
+#endif
