68e8ed219a357314cfe19a2014e3df73d38516fb
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-02-10 23:35:37 +0100

    Create exercise10.1.print.h

---
+++ BeginningCplusplus_2014_Horton/chap10/exercise10.1.print.h
@@ -0,0 +1,7 @@
+#ifndef PRINT_H
+#define PRINT_H
+#include <iostream>
+#include <string>
+void print(const std::string &s);
+#endif
+// for definition of this functon see file exercise10.1.print.cpp
