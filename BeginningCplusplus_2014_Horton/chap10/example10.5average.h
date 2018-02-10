03df83330497b5f7fc0d8aaf378baa85ffdb1a09
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-02-10 23:54:34 +0100

    Create example10.5average.h

---
+++ BeginningCplusplus_2014_Horton/chap10/example10.5average.h
@@ -0,0 +1,15 @@
+// example10.5average.h
+#ifndef AVERAGE_H
+#define AVERAGE_H
+#include <type_traits> // provides : is_arithmetic< >
+#include <vector>
+ 
+template<class T>  T func_average(const std::vector<T> &values)
+{
+	static_assert(std::is_arithmetic<T>::value, "Type parameter for average() must be arithmetic.");
+	T sum {};
+	for(auto& value : values)
+		sum += value;
+	return sum/values.size();
+}
+#endif
