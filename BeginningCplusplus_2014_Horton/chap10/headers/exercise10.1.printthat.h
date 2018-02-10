f02288b6e6b1b2eb9b36b07f524ebf87c3375775
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-02-10 23:44:42 +0100

    Create exercise10.1.printthat.h

---
+++ BeginningCplusplus_2014_Horton/chap10/headers/exercise10.1.printthat.h
@@ -0,0 +1,7 @@
+#ifndef PRINTTHAT_H
+#define PRINTTHAT_H
+#include <iostream>
+#include <string>
+void print_that(const std::string &s);
+#endif
+// for definition of this functon see file exercise10.1.print.cpp
