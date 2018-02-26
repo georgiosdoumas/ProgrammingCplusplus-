5300a0685d5db4b3c66f6c3ffea757a462b2da4c
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-02-26 23:24:30 +0100

    Update example14.01.toughPack.h

--- BeginningCplusplus_2014_Horton/chap14/example14.01.toughPack.h
+++ BeginningCplusplus_2014_Horton/chap14/example14.01.toughPack.h
@@ -1,16 +1,13 @@
 #ifndef TOUGHPACK_H
 #define  TOUGHPACK_H
-
 #include  "example14.01.box.h"
 
 class  ToughPack : public Box
 {
-public:
+ public:
   // Constructor
   ToughPack(double lv, double wv, double hv) : Box {lv, wv, hv} {}
-
   // Function  to  calculate volume of  a ToughPack  allowing 15%  for  packing
   double volume()  const { return 0.85*length*width*height; }
-  
 };
 #endif
