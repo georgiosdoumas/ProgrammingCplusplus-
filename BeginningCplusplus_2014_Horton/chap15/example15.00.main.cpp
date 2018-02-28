7513bb2a56c27ee20b11e1fc8637d25a9c5d716a
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-02-28 14:06:19 +0100

    Update example15.00.main.cpp

--- BeginningCplusplus_2014_Horton/chap15/example15.00.main.cpp
+++ BeginningCplusplus_2014_Horton/chap15/example15.00.main.cpp
@@ -3,22 +3,24 @@
 int main()
 {
   int test;
-  std::cout << "Enter a integer smaller than 5 :";
+  std::cout << "Enter a integer smaller than 6 :";
   while(std::cin >> test)
-   try
-   {
-    if(test > 5)
-      throw " Input is greater than 5"; // Throws an exception of type const char*
-    std::cout << "An exception was not thrown! Good input.\n";
-    break;
-   }
-   catch(const char* message)
-   {
-     //Code to handle the exception,executes if exception of type 'char*' or 'const char*' is thrown
-    std::cout << message << std::endl;
-    std::cout << " Please enter a valid input.Integer smaller than 5:";
-   }
-  //We could have enclosed the previews code in {} but it is not strictly needed on this case
-  std::cout << "So lets see what we can do with the good input!" << std::endl;
-  std::cout << "Its square is " << test*test << std::endl;
+    try
+    {
+      if(test > 5)
+        throw " Input is greater than 5"; // Throws an exception of type const char*
+      std::cout << "An exception was not thrown! Good input.\n";
+      std::cout << "So lets see what we can do with the good input!" << std::endl;
+      std::cout << "Its square is " << test*test << std::endl;
+      return 0;
+    }
+    catch(const char* message)
+    {
+  //Code to handle the exception,executes if exception of type 'char*' or 'const char*' is thrown
+      std::cout << message << std::endl;
+      std::cout << " Please enter a valid input.Integer smaller than 5:";
+    }
+  // if we reach here, then the result of cin>>test was not successful
+    std::cout << "You did not even provided a number! I will not tolerate this. Bye." << std::endl;
 }
+
