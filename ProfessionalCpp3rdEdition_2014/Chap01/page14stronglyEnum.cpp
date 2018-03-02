bc82b5f8463cd20f0f1ebf7c334ed793a15e0337
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-03-02 15:24:26 +0100

    Update page14stronglyEnum.cpp

--- ProfessionalCpp3rdEdition_2014/Chap01/page14stronglyEnum.cpp
+++ ProfessionalCpp3rdEdition_2014/Chap01/page14stronglyEnum.cpp
@@ -7,10 +7,10 @@ int main()
     enum class myCharEnum: char { enumchar_d = 'd' , enumchar_f = 'f' }; //A variation of what is done on top of page 15
     MyEnum variable1 = MyEnum::EnumValue3;
     //cout << variable1 << endl;  //Now this simple way does not compile.
+    // but we can use the static_cast<type>(variable) that was presented on page 13 :
     cout << static_cast<int>(variable1) << " is the value for variable1." << endl;   
     myCharEnum leter1 = myCharEnum::enumchar_f ;
     cout << static_cast<char>(leter1) << endl;      
-    //As we said on page14a_typedef_enums.cpp, static_cast is mentioned on Chapter10. 
     return 0;
 }
 // g++ -Wall -std=c++11 -o page14stronglyEnum  page14stronglyEnum.cpp
