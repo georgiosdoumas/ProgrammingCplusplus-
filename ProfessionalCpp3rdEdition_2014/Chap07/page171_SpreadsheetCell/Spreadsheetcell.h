d923afe1514cbd189945b419614343878ceada27
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-03-23 14:57:34 +0100

    Create Spreadsheetcell.h

---
+++ ProfessionalCpp3rdEdition_2014/Chap07/page171_SpreadsheetCell/Spreadsheetcell.h
@@ -0,0 +1,26 @@
+#ifndef CELL
+#define CELL
+#include <string>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+class SpreadsheetCell
+{
+private:
+  double mValue;
+  string mString;
+  string doubleToString(double inValue) const;   // private helper function
+  double stringToDouble(const string& inString) const;  // private helper function
+public:
+  SpreadsheetCell(double initialValue);             // constructor for double values passed 
+  SpreadsheetCell(const string& initialString);     // constructor for strings passed as ref
+  SpreadsheetCell();                               //default constructor
+  SpreadsheetCell(const SpreadsheetCell& src);     //copy constructor
+  SpreadsheetCell& operator=(const SpreadsheetCell& rhs);
+  void setValue(double inValue);
+  double getValue() const;
+  void setString(const string& inString);
+  const string& getString() const;
+};
+#endif
