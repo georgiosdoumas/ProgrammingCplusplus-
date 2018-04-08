20b447a5a9e37ec6894ebddcf580514c7675a888
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-04-09 00:03:52 +0200

    Update Exercise9.04.cpp

--- CplusplusPrimer_5thEd_2012/chapter09/Exercise9.04.cpp
+++ CplusplusPrimer_5thEd_2012/chapter09/Exercise9.04.cpp
@@ -7,18 +7,20 @@ using std::vector;
 
 bool searchInt(vector<int>::iterator startIter, vector<int>::iterator endIter, int findMe )
 {
-	if( startIter > endIter ) return false; //if the parameters are bad, do not get into a loop that would be endless!
-	for(auto iteratorElement = startIter; iteratorElement != endIter; ++iteratorElement) {
+  if( startIter > endIter ) //Thought it would be dangerous to not have it (startIter >= endIter), for cases of empty vectors, but it's OK
+    return false; //if the parameters are bad, do not get into a loop that would be endless!
+  for(auto iteratorElement = startIter; iteratorElement != endIter; ++iteratorElement) {
        cout << *iteratorElement;
        if( *iteratorElement == findMe ) { cout << endl; return true; }
        else cout << " ";
-	} 
-	cout << endl;
-	return false;
+  } 
+  cout << endl;
+  return false;
 }
 int main() 
 {
 	vector<int> numbers { 4, -3, 7, 12, 9, 123, 56};
+	vector<int> empty;
 	auto initial = numbers.begin();
 	auto final = numbers.end();
 	int search1 = 7, search2 = 13;
@@ -29,12 +31,21 @@ int main()
 	if(searchInt(initial, final, search2)) cout << "  Found it!" << endl;
 	else cout << "  It does not exist." << endl;
 	cout << "Some partial vector searches:" << endl;
-	if(searchInt(initial+3, final-1, search1)) cout << "  Found it!" << endl;
-	else cout << search1 << " does not exist in that part of the vector." << endl;
-	if(searchInt(initial+3, final-3, 12)) cout << "  Found it!" << endl;
-	else cout << " 12 does not exist in that part of the vector." << endl;
-	if(searchInt(initial+3, final-4, 12)) cout << "  Found it!" << endl;
-	else cout << " 12 does not exist in that part of the vector." << endl;
+	if(searchInt(initial+3, final-1, search1)) 
+	  cout << "  Found it!" << endl;
+	else 
+	  cout << search1 << " does not exist in that part of the vector." << endl;
+	if(searchInt(initial+3, final-3, 12)) 
+	  cout << "  Found it!" << endl;				// we get this output
+	else 
+	  cout << " 12 does not exist in that part of the vector." << endl;
+	if(searchInt(initial+3, final-4, 12)) 
+	  cout << "  Found it again!" << endl;
+	else 
+	  cout << " :12 does not exist in that part of the vector." << endl;  // we get this
+	if( searchInt(empty.begin(), empty.end(), 0) )
+	  cout << "  Found it!" << endl;
+	else cout << "  Does not exist, or you gave me an empty vector!" << endl;  // we get this output
 	return 0;
 }
 // g++ -Wall -std=c++11 -o Exercise9.04 Exercise9.04.cpp 
