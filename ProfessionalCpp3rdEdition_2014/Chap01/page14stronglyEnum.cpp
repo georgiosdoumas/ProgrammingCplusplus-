be402d1d2accbc4271f3a1c8519ccb603c89645a
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-04-10 00:42:43 +0200

    Update page14stronglyEnum.cpp

--- ProfessionalCpp3rdEdition_2014/Chap01/page14stronglyEnum.cpp
+++ ProfessionalCpp3rdEdition_2014/Chap01/page14stronglyEnum.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 int main()
 {
-    enum class MyEnum { EnumValue1, EnumValue2 = 10, EnumValue3 };
+    enum class MyStrongEnum { EnumValue1, EnumValue2 = 10, EnumValue3 };   // 0, 10, 11
     enum class myCharEnum: char { enumchar_d = 'd' , enumchar_f = 'f' }; //A variation of what is done on top of page 15
-    MyEnum variable1 = MyEnum::EnumValue3;
+    MyStrongEnum variable1 = MyStrongEnum::EnumValue3;
     //cout << variable1 << endl;  //Now this simple way does not compile.
     // but we can use the static_cast<type>(variable) that was presented on page 13 :
     cout << static_cast<int>(variable1) << " is the value for variable1." << endl;   
