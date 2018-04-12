41a9cd9ac3d45f60f5b3f6f0b25ebf62411d658b
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-04-13 00:21:20 +0200

    Create page37.passenger.h

---
+++ Data.Structures.and.Algorithms.in.Cpp.2ndEdition.2011/chapter01/page37.passenger.h
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+enum MealType { NO_PREF, REGULAR, LOW_FAT, VEGETARIAN };
+
+class Passenger 		// Passenger (as a class)
+{
+  public:
+	 Passenger();        // default constructor
+	 Passenger(const string& nm, MealType mp, long int ffn = 0);
+	 Passenger(const Passenger& pass); // copy constructor
+	
+	 void setName(string strName) { name = strName; }	
+	// now all the following geters were required for the operator<<
+	// another solution would be to declare the operator<<() as a friand function, as explained on page 43
+	 string getName() const {return name;}
+	 MealType getMealpref() const { return mealPref; }
+	 long int getFfn() const { return freqFlyerNo;}
+   bool getFfstatus() const { return isFreqFlyer; }
+	
+	 bool isFrequentFlyer() const;   // is this a frequent flyer?
+   void makeFrequentFlyer(long int newFreqFlyerNo); // make this a frequent flyer
+   bool operator==( const Passenger& rhsPass) const;  //overloading the equality operator ==
+// . . . other member functions
+  private:
+	 string name;		// passenger name
+	 MealType mealPref;		// meal preference
+	 bool isFreqFlyer;		// is a frequent flyer?
+	 long int freqFlyerNo;		// frequent flyer number
+};
+//The output operator can still be defined outside Class body
+ostream& operator<<(ostream& outstrm, const Passenger& pass) ;
+
+
