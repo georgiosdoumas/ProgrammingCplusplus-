9c12b73a00e36a4c88b48810261638fdff2e80ea
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-04-21 16:05:28 +0200

    Create page03VlassOld.cpp

---
+++ Data.Structures.and.Algorithms.in.Cpp.4thEd.2013.Drozdek/chap01/page03VlassOld.cpp
@@ -0,0 +1,20 @@
+#include "page3ClassOld.h"
+
+C_old::C_old(char *s, int i, double d) 
+{
+    strcpy(dataMember1,s);
+    dataMember2 = i;
+    dataMember3 = d;
+}
+
+void C_old::memberFunction1()
+{
+	 dataMember2++; dataMember3--; //potentially this function could change data members
+   cout << dataMember1 << ' ' << dataMember2 << ' ' << dataMember3 << endl;
+}
+
+void C_old::memberFunction2(int i, const char *s)
+{
+    dataMember2 = i;
+    cout << i << " received from " << s << endl;
+}
