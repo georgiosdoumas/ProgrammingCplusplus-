865628a5d0e408cbb1f2c175a69d3b77ef660831
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-04-21 16:13:42 +0200

    Create page03main.cpp

---
+++ Data.Structures.and.Algorithms.in.Cpp.4thEd.2013.Drozdek/chap01/page03main.cpp
@@ -0,0 +1,16 @@
+#include "page3ClassOld.h"
+int main() 
+{
+	C_old object1("object1",100,2000), object2("object2"), object3;
+	object1.memberFunction1();
+	object2.memberFunction1();
+	object3.memberFunction1();
+	
+	object1.memberFunction2(123);
+	object1.memberFunction2(123, "object1");
+	object1.memberFunction1();
+}
+
+// /usr/bin/g++ -Wall -std=c++11 -o page3 page3ClassOld.cpp page3main.cpp
+// /usr/bin/g++ -Wall -std=c++03 -o page3x03 page3ClassOld.cpp page3main.cpp
+// the same result for both c++11 and c++03 , warnings about the char* s 
