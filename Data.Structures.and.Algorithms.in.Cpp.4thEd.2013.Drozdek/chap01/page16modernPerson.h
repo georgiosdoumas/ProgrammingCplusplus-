2562cffc2cbaa65cc066a0f40bcb19e78aaf01c7
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-04-22 15:15:14 +0200

    Create page16modernPerson.h

---
+++ Data.Structures.and.Algorithms.in.Cpp.4thEd.2013.Drozdek/chap01/page16modernPerson.h
@@ -0,0 +1,44 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class modernPerson
+{
+  private:   //of course it would be simpler to declare: string name;
+    string* pname = nullptr;  //...We are only doing this with pointer, to present the need for copy constructor
+    int age;						
+  public:
+    modernPerson(string nm = "NONAME", int a = 1) //a constructor with 2 default parameter values
+    {
+  	    pname = new string{nm};
+	    age = a;
+    }
+    string& getname() const { return *pname;}
+    int getage() const { return age;}
+    void setname(const string& nm ) { *pname = nm; }
+    void setage(int ag) { age = ag; }
+    modernPerson(const modernPerson& mp) 	// copy constructor
+    { 
+    		pname = new string;
+         *pname = *(mp.pname);
+         age = mp.age;
+    }
+	 modernPerson& operator=(const modernPerson& mp) // assignment operator 
+	 {
+	   if (this != &mp) // no assignment to itself;
+	   { 
+	     if (pname != nullptr)
+	         { delete pname; pname = nullptr; }
+	     pname = new string;
+       *pname = *(mp.pname);
+	     age = mp.age;
+	   }
+	   return *this;
+	}    
+};
+
+ostream& operator<<(ostream& outstrm, const modernPerson& mp)
+{
+	outstrm << mp.getname() << ' ' << mp.getage() << endl;
+	return outstrm;
+}
