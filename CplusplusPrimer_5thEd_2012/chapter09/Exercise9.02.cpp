5e4c479439ad7b277f91fd3deb592002efdf1067
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-07-09 11:10:42 +0200

    Update Exercise9.02.cpp

--- CplusplusPrimer_5thEd_2012/chapter09/Exercise9.02.cpp
+++ CplusplusPrimer_5thEd_2012/chapter09/Exercise9.02.cpp
@@ -1,10 +1,17 @@
 /* Exercise 9.2: Define a list that holds elements that are deques that hold ints. */
 #include <list>
 #include <deque>
+#include <iostream>
 
 int main()
 {
   std::list <std::deque<int>> my_emptylist_of_deque_ints;
-  std::list <std::deque<int>> my_list_of_deque_ints(5); // a 5 element list
+  std::list <std::deque<int>> my_list_of_deque_ints(5); // a 5 element list, each element is an empty deque 
+  //lets try some simple operations form  Table 9.1 page 330 :
+  std::cout << "The first list of deques has a size of : " << my_emptylist_of_deque_ints.size() << std::endl;
+  if( !my_list_of_deque_ints.empty() )
+    std::cout << "The second list of deques has a size of : "<< my_list_of_deque_ints.size() << std::endl;
   return 0;
 }
+
+// g++ -Wall -std=c++11 -o Exercise9.02 Exercise9.02.cpp 
