8c656323607ae6a758d798840d82749f57893499
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-07-09 13:33:05 +0200

    Update Exercise9.05.cpp

--- CplusplusPrimer_5thEd_2012/chapter09/Exercise9.05.cpp
+++ CplusplusPrimer_5thEd_2012/chapter09/Exercise9.05.cpp
@@ -1,4 +1,6 @@
-/* Exercise 9.5: Rewrite the previous program to return an iterator to the requested element. 
+/* Exercise 9.5: Rewrite the previous program 
+[Exercise 9.4: Write a function that takes a pair of iterators to a vector<int> and an
+int value. Look for that value in the range] to return an iterator to the requested element. 
 Note that the program must handle the case where the element is not found. */
 #include <iostream>
 #include <vector> 
