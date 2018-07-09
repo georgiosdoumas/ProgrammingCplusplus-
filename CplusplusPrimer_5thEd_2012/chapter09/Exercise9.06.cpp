af62d361f538e2864cf7a3f8a5c2ea5f89f0f32b
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-07-09 14:49:41 +0200

    Update Exercise9.06.cpp

--- CplusplusPrimer_5thEd_2012/chapter09/Exercise9.06.cpp
+++ CplusplusPrimer_5thEd_2012/chapter09/Exercise9.06.cpp
@@ -12,10 +12,20 @@ int main()
 	list<int>::iterator iter1 = lst1.begin(), iter2 = lst1.end();
 	while (iter1 != iter2)   // comparison operator < does not work for list iterators, only for vector,string,deque,array
 	{
-		cout << *iter1 << " ";
+		*iter1 +=2; // just to show that since we do not declared const_iterator, we are allowed to change the value of element
+		//cout << *iter1 << " ";
 		++iter1;
 	}
 	cout << endl;
+	// To only output the elements, it is safer to code with const_iterators 
+	list<int>::const_iterator citer1 = lst1.cbegin(), citer2 = lst1.cend();
+	while (citer1 != citer2)
+	{
+		cout << *citer1 << " ";
+		++citer1;
+	}
+	cout << endl;
+	
 	return 0;
 }
 // g++ -Wall -std=c++11 -o Exercise9.06 Exercise9.06.cpp 
