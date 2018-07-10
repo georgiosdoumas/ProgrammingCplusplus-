c66583f01cb22f747ceb456bb85b5c7a4a89d5b8
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-07-10 09:31:47 +0200

    Update Exercise9.16.cpp

--- CplusplusPrimer_5thEd_2012/chapter09/Exercise9.16.cpp
+++ CplusplusPrimer_5thEd_2012/chapter09/Exercise9.16.cpp
@@ -11,23 +11,25 @@ bool listvec_isequal(vector<int> ivecleft, list<int> ilistright)
 	list<int>::const_iterator literr = ilistright.cbegin();
 	while ( viterl != ivecleft.cend() )  // I could chose the literr , since vector and list have the same size 
 	{
-		if( *viterl != *literr ) return false;
-		++viterl;
-		++literr;
+	    if( *viterl != *literr ) return false;
+	    ++viterl;
+	    ++literr;
 	}
 	return true;
 }
 
 int main()
 {
-	vector<int> v1 = { 1, 3, 5, 7, 9, 12 };
+   vector<int> v1 = { 1, 3, 5, 7, 9, 12 };
+   list<int> l0;
    list<int> l2 = { 1, 3, 9 };
    list<int> l3 = { 1, 3, 5, 7 };
    list<int> l4 = { 1, 3, 5, 7, 9, 12 };
+   cout << listvec_isequal(v1,l0) << endl;
    cout << listvec_isequal(v1,l2) << endl;
    cout << listvec_isequal(v1,l3) << endl;
    cout << listvec_isequal(v1,l4) << endl;  // only this is true , 1 
-	return 0;
+   return 0;
 }
 
 // /usr/bin/g++ -Wall -std=c++11 -o Exercise9.16 Exercise9.16.cpp 
