6073c3436580fc2dd6cfd05d7878a086897177d9
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-07-12 12:52:39 +0200

    Update Exercise8.06.main.cpp

--- CplusplusPrimer_5thEd_2012/chapter08/Exercise8.06.main.cpp
+++ CplusplusPrimer_5thEd_2012/chapter08/Exercise8.06.main.cpp
@@ -6,7 +6,7 @@ transactions from a file. Pass the name of the file as an argument to main (§ 6
 
 int main(int argc, char * argv[]) 
 {
-	 Sales_data just_isbn_book("123456789-D");               //test usage of partial constructor.OK it compiles
+    Sales_data just_isbn_book("123456789-D");               //test usage of partial constructor.OK it compiles
     print(cout , just_isbn_book) ; cout << endl;
     Sales_data full_book("123456789-F", 3, 38.8, "Effective C++"); //test usage of full constructor.OK it compiles 
     print(cout , full_book) ; cout << endl;
@@ -16,42 +16,42 @@ int main(int argc, char * argv[])
     	 Sales_data total(cin);  //using the constructor for initialization: reading input e.g. from a file, or from console
     	 if( !total.isbn().empty() ) 
     	 { 
-         Sales_data trans;           // variable to hold data for the next transaction
-         while( read(cin, trans) ) 
-         {
-            if ( total.isbn() == trans.isbn() ) total.combine(trans);  
-            else 
-            {                                  
+           Sales_data trans;           // variable to hold data for the next transaction
+           while( read(cin, trans) ) 
+           {
+              if ( total.isbn() == trans.isbn() ) total.combine(trans);  
+              else 
+              {                                  
                 print(cout , total ) ; cout<<endl; 
                 total = trans ;                
-            }
-          }
-        print( cout, total ) ; cout<<endl;     
-       } 
-       else std::cerr << "No data?!" << endl;   
+              }
+           }
+           print( cout, total ) ; cout<<endl;     
+         } 
+         else std::cerr << "No data?!" << endl;   
      }
      else if (argc == 2) // input file passed as command line arg
      {
-     		ifstream input_file(argv[1]);
-     		Sales_data total;
-     		if (read(input_file, total)) 
-     		{ // read the first transaction
-				Sales_data trans;		// variable to hold data for the next transaction
-				while(read(input_file, trans)) 
-				{							// read the remaining transactions
-					if (total.isbn() == trans.isbn()) // check isbn s
-					  total.combine(trans); // update the running total
-					else 
-					{
-						print(cout, total) << endl; // print the results
-						total = trans;					// process the next book
-					}
-				}
-				print(cout, total) << endl; // print the last transaction
-			} else									// there was no input
-			cerr << "No data?!" << endl;
+     	ifstream input_file(argv[1]);
+     	Sales_data total;
+     	if (read(input_file, total)) 
+     	{ // read the first transaction
+		Sales_data trans;		// variable to hold data for the next transaction
+		while(read(input_file, trans)) 
+		{							// read the remaining transactions
+		        if (total.isbn() == trans.isbn()) // check isbn s
+			    total.combine(trans); // update the running total
+			else 
+			{
+				print(cout, total) << endl; // print the results
+				total = trans;					// process the next book
+			}
+		}
+		print(cout, total) << endl; // print the last transaction
+	} else									// there was no input
+	    cerr << "No data?!" << endl;
      }
-	return 0;
+     return 0;
 }
 // g++ -Wall -std=c++11 -o Exercise8.6 Exercise8.06.functions.cpp Exercise8.06.main.cpp 
 /* 
