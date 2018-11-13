63548f015d775ec7f48d7c11c00bf8e1032197e4
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2018-11-13 23:48:49 +0100

    Update page89.series.h

--- Data.Structures.and.Algorithms.in.Cpp.2ndEdition.2011/chapter02/page89.series.h
+++ Data.Structures.and.Algorithms.in.Cpp.2ndEdition.2011/chapter02/page89.series.h
@@ -5,12 +5,12 @@ class Progression    // abstract base class (cannot create objects of it)
 {
   public:
 	Progression(long f = 0): first{f}, cur{f} { } //constructor with default value for first element=0
-	virtual ~Progression() { };						// destructor, good to have one for classes with virtual functions
+	virtual ~Progression() { };			// destructor, good to have one for classes with virtual functions
 	void printProgression(int n);
   protected:
 	virtual long firstValue();
-	virtual long nextValue() = 0;			// pure virtual function (no need to define it on the .cpp file)
-  protected:   // not private! Derived classes will need to access them!
+	virtual long nextValue() = 0;		// pure virtual function (no need to define it on the .cpp file)
+  protected:   // not private! Because derived classes will need to access them!
 	long first;  // this first element will always be provided to all derived classes. No need to have it again
 	long cur;    // the current element also will always be provided to derived classes. 
 };
@@ -18,21 +18,21 @@ class Progression    // abstract base class (cannot create objects of it)
 class ArithProgression : public Progression  // derived class : arithmetic progression 
 { 
   public:
-    ArithProgression(long i = 1);			// constructor, default increment 1
+    ArithProgression(long i = 1);		// constructor, default increment 1
   protected:
-    virtual long nextValue();					// advance to next value
-  private:										 // but this can be private. The book example still keeps it protected
-    long inc;									// inc is the distance between 2 successive values
+    virtual long nextValue();			// advance to next value
+  private:					// but this can be private. The book example still keeps it protected
+    long inc;					// inc is the distance between 2 successive values
 };
 
 class GeomProgression : public Progression  // derived class : geometric progression
 { 
   public:
-	 GeomProgression(long b = 2);			// constructor with default multiplier 
+	 GeomProgression(long b = 2);		// constructor with default multiplier 
   protected:
 	 virtual long nextValue();         // advance
-  private:			  //again, this can be private (if there will be no derived classes form Geom no need to have it protected)
-    long base;						       // base value
+  private:	  //again, this can be private (if there will be no derived classes form Geom no need to have it protected)
+    long base;				// base value
 };
 
 class FibonacciProgression : public Progression  // derived class : Fibonacci progression
@@ -43,7 +43,7 @@ class FibonacciProgression : public Progression  // derived class : Fibonacci pr
     virtual long firstValue();			// reset
     virtual long nextValue();         // advance
   private:
-    long second;					// second value
-    long prev;  					// previous value. So in total we have as data members: first,cur,second,prev 
+    long second;			// second value
+    long prev;  			// previous value. So in total we have as data members: first,cur,second,prev 
 };
 #endif
