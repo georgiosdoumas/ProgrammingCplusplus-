9f37fe8ddbdb905ed6e27c13841b6ca5d0ef244c
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2023-07-01 19:44:39 +0200

    vector name change

--- HackerRanckExercises/EfficientJanitor.cpp
+++ HackerRanckExercises/EfficientJanitor.cpp
@@ -4,29 +4,29 @@
 #include <algorithm>
 using std::vector; using std::cout; using std::endl;
 
-int garbageCollector(vector<float> v) {
+int garbageCollector(vector<float> loads) {
     const float maxWeight = 3.00;
     int number_of_trips = 0;
-    if(v.size() == 0)
+    if(loads.size() == 0)
         return number_of_trips;
-    sort(v.begin(), v.end());
-    int start = 0, end = v.size();
+    sort(loads.begin(), loads.end());
+    int start = 0, end = loads.size();
     while(start < end) {
         end--;  //for sure the janitor will pick the heaviest element
         number_of_trips++;  // but possibly in the same trip ...
-        if((v[start] + v[end]) <=  maxWeight)  // ... he might be able to carry also one of the light elements
+        if((loads[start] + loads[end]) <=  maxWeight)  // ... he might be able to carry also one of the light elements
             start++;
     }
     return number_of_trips;
 }
 
 // passing the maximum load for 1 trip as parameter, and using iterators instead of index
-int garbageCollector(vector<float> v, const float maxWeight) {
+int garbageCollector(vector<float> loads, const float maxWeight) {
     int number_of_trips = 0;
-    if(v.size() == 0)
+    if(loads.size() == 0)
         return number_of_trips;
-    sort(v.begin(), v.end());
-    vector<float>::iterator iter = v.begin(), stopiter = v.end();
+    sort(loads.begin(), loads.end());
+    vector<float>::iterator iter = loads.begin(), stopiter = loads.end();
     while(iter != stopiter) {
         --stopiter;
         number_of_trips++;
